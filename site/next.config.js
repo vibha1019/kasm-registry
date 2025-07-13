12374a8e9cb7a2b2724e11d724cb846472bd36ef
Vibha Mandayam <114361148+vibha1019@users.noreply.github.com>
2025-07-13 00:17:18 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -1,21 +1,17 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   output: 'export',
   distDir: 'out',
   env: {
     name: 'Kasm Technologies',
     description: 'Me testing something.',
-    icon: 'https://vibha1019.github.io/kasm-registry/1.1/icon.png',
+    icon: 'https://vibha1019.github.io/kasm-registry/1.0/icon.png',
     listUrl: 'https://vibha1019.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.1',
+  basePath: '/kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
   }
 }
-
-module.exports = nextConfig;
